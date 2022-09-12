b13150fd631d18521d5567a2dfd303cd56bca16a
ZeusOnline1998 <zeusonline1998@gmail.com>
2022-09-12 22:07:29 +0530

    Created Insert Function

--- Java DBMS/src/mysqljdbc/mysqldao.java
+++ Java DBMS/src/mysqljdbc/mysqldao.java
@@ -1,14 +1,12 @@
 package mysqljdbc;
 import java.sql.*;
-
+import java.util.*;
 
 public class mysqldao {
 	
 	static EmployeeJava DAO = new EmployeeJava();
 	
-	public static void getEmployee(int emp_id) {
-		
-		
+	public static void getEmployee(int emp_id) {	
 		
 		DAO.connect();
 		
@@ -20,11 +18,41 @@ public class mysqldao {
 				"\nDepartment: "+e1.getDepartment()+
 				"\nSalary: "+e1.getSalary());
 	}
+	
+	public static void addEmployee() {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String emp_name, email, department;
+		int salary;
+		System.out.print("Employee Name: ");
+		emp_name = sc.next();
+		System.out.print("\nEmail: ");
+		email = sc.next();
+		System.out.print("\nDepartment: ");
+		department = sc.next();
+		System.out.print("\nSalary: ");
+		salary = sc.nextInt();
+		
+		Employee e1 = new Employee();
+		e1.setEmp_name(emp_name);
+		e1.setEmail(email);
+		e1.setDepartment(department);
+		e1.setSalary(salary);
+		
+		DAO.connect();
+		
+		DAO.addEmployee(e1);
+	}
+	
+	
 	public static void main(String[] args) throws Exception{
 		
 		int emp_id = 1;
 		
-		getEmployee(emp_id);
+//		getEmployee(emp_id);
+		
+		addEmployee();
 	}
 }
 
@@ -35,14 +63,14 @@ class EmployeeJava{
 	private String url = "jdbc:mysql://localhost:3306/dbcompany";
 	private String user = "root";
 	private String pass = "pass";
-	private String employeeJavaTable = "employee_info";
+	private String employeeJavaTable = "employeejavatable";
 	public void connect() {
 		
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");  // Can be ignored since it is always initialized in the latest versions.
 			con = DriverManager.getConnection(url, user, pass);
 			String ctable = "CREATE TABLE IF NOT EXISTS " +this.employeeJavaTable
-					+ " (emp_id int(10),"
+					+ " (emp_id int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,"
 					+ " emp_name varchar(50),"
 					+ " email varchar(100),"
 					+ " department varchar(20),"
@@ -65,7 +93,7 @@ class EmployeeJava{
 			
 			Employee e1 = new Employee();
 			
-			String query = "Select * from "+this.employeeJavaTable+" where id="+emp_id+";";
+			String query = "Select * from "+this.employeeJavaTable+" where emp_id="+emp_id+";";
 //			String query = "Select * from "+this.employeeJavaTable+" where emp_id = "+emp_id+";";
 //			
 //			String query = "Select * from employee_info where id = 1";
@@ -98,6 +126,33 @@ class EmployeeJava{
 		}
 		return null;
 			
+	}
+	
+	public void addEmployee(Employee e1) {
+		
+		try {
+			String query = "Insert into "+this.employeeJavaTable+"(emp_name, email, department, salary)"
+					+ " values (?,?,?,?);";
+		
+			PreparedStatement stmt = con.prepareStatement(query);
+			
+			stmt.setString(1, e1.getEmp_name());
+			stmt.setString(2, e1.getEmail());
+			stmt.setString(3, e1.getDepartment());
+			stmt.setInt(4, e1.getSalary());
+			
+			try {
+				stmt.executeUpdate();
+				System.out.println("Data Inserted");
+				
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	
